e1403a8d07c8a656cfc96e5c74b751983e01eb05
Ricardo Júnior <rickalvesjunior@outlook.com>
2023-04-04 18:29:33 -0300

    Aprendendo a mexer com arrays de duas dimensões

---
+++ OneDrive/Área de Trabalho/Estagiario deObra/Arrays Js/lista2dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias]
+console.log(`A aluna da posição 1 da Lista é: ${listaDeAlunosEMedias[0][1]}.
+A Média final desse aluno é: ${listaDeAlunosEMedias[1][1]}`)
