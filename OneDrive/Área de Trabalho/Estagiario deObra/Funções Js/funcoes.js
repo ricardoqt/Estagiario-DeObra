cb6d120c5b49e3e36dfa0f76b54a395f614718df
Ricardo Júnior <rickalvesjunior@outlook.com>
2023-04-08 21:42:11 -0300

    Funções

---
+++ OneDrive/Área de Trabalho/Estagiario deObra/Funções Js/funcoes.js
@@ -0,0 +1,17 @@
+
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+imprimeTexto("Oiiee")
+imprimeTexto("Tudo bem?")
+
+// Três formas de escrever funções
+
+function soma (){
+    return 2+2
+}
+
+console.log (soma())
+
+imprimeTexto(soma())
