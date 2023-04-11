704637e330ae0243f6710e8be01954b92dedb8b1
Ricardo Júnior <rickalvesjunior@outlook.com>
2023-04-11 13:26:49 -0300

    Expressões em javaScript

---
+++ OneDrive/Área de Trabalho/Estagiario deObra/Funções Js/expressao.js
@@ -0,0 +1,9 @@
+function minhaFuncao(param){
+    // bloco de código
+}
+minhaFuncao("param")
+
+//expressão de função
+
+const soma = function(num1, num2){return num1 + num2}
+console.log(soma(1,2))
